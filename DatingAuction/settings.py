026e447dddec9b5a65cae7078268bb8bccfacdd3
urkarnad <d.hrechko@ukma.edu.ua>
2025-12-13 19:45:33 +0200

    something added

--- DatingAuction/settings.py
+++ DatingAuction/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+
 
 
 load_dotenv()
