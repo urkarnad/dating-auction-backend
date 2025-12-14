939a482ea828ba7d2645b28566a4152118f43d95
kinetique <sonyacat0423@gmail.com>
2025-12-15 00:19:19 +0200

    fixed login

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from user.views import logout_view, auth_success, RegisterView
+from user.views import logout_view, auth_success, RegisterView, LoginView
 
 urlpatterns = [
     path('auth/', include('social_django.urls', namespace='social')),
     path('auth/success/', auth_success, name='auth_success'),
     path('logout/', logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
